f192658c2ae7dceda2785e831652ef4a50671dbe
yuan666 <839972913@qq.com>
2022-05-27 17:16:01 +0800

    add:xxxx;GeeCached的Day2添加

---
+++ GeeCache/Day2/geecache.go
@@ -0,0 +1,50 @@
+package Day2
+
+import (
+	"sync"
+)
+
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+	g := groups[name]
+
+	return g
+}
---
+++ GeeCache/Day2/mutexTest.go
@@ -0,0 +1,33 @@
+package Day2
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var m sync.Mutex
+var set = make(map[int]bool, 0)
+
+func printOnce(num int) {
+	m.Lock()
+	defer m.Unlock()
+
+	if _, exist := set[num]; !exist {
+		fmt.Println(num)
+	}
+	set[num] = true
+}
+
+func MutextTest()  {
+	for i:=0;i<10;i++ {
+		go printOnce(100)
+		time.Sleep(time.Microsecond*10)
+	}
+	time.Sleep(time.Second)
+
+}
+
+func main()  {
+	MutextTest()
+}
